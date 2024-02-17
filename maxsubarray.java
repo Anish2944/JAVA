2cb131ef4ad6aea1071c5c7b7d8e80c74299715a
ANISH KUSHWAHA <69107802+Anish2944@users.noreply.github.com>
2024-02-17 20:49:55 +0530

    user input added

--- maxsubarray.java
+++ maxsubarray.java
@@ -1,4 +1,7 @@
 // Kadane's Algorithm
+
+import java.util.Scanner;
+
 public class maxsubarray {
     public static void MSA(int number[]) {
         int cs = 0;
@@ -11,12 +14,21 @@ public class maxsubarray {
             cs = Math.max(number[i], cs + number[i]);
             ms = Math.max(cs, ms);
         }
-        System.out.println("Max number: " + ms);
+        System.out.println("Max SubArray Sum: " + ms);
     }
 
     public static void main(String args[]) {
-        int number[]= {-2,-3,4,-1,-2,1,5,-3};
+        // int number[]= {-2,-3,4,-1,-2,1,5,-3};
         // int number[] = { -10, -20, -30, -40, -50 };
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int size = sc.nextInt();
+        int[] number = new int[size];
+        System.out.println("Enter all elements");
+        for (int i = 0; i < number.length; i++) {
+            number[i] = sc.nextInt();
+        }
         MSA(number);
+        sc.close();
     }
 }
