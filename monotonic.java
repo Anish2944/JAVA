d20474eef64957d56a79da921a0273e3a7bee464
ANISH KUSHWAHA <69107802+Anish2944@users.noreply.github.com>
2024-03-24 19:54:19 +0530

    renamed

--- monotonic.java
+++ monotonic.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
-public class arrlist {
-    public static boolean monotonic(ArrayList<Integer> list) {
+public class monotonic {
+    public static boolean monotoniChecker(ArrayList<Integer> list) {
         if (list.get(0) < list.get(1)) {
             for (int i = 0; i < list.size() - 1; i++) {
                 if (list.get(i) > list.get(i+1)) {
@@ -25,7 +25,7 @@ public class arrlist {
         list.add(4);
         list.add(3);
         list.add(3);
-        if (monotonic(list) == true) {
+        if (monotoniChecker(list) == true) {
             System.out.println("monotonic");
         } else {
             System.out.println("not Monotonic");
