94291e381c371a1a9794b8320f295228e436314d
ANISH KUSHWAHA <69107802+Anish2944@users.noreply.github.com>
2024-03-24 21:22:10 +0530

    new code

--- lonely.java
+++ lonely.java
@@ -1,3 +1,41 @@
+import java.util.ArrayList;
+
 public class lonely {
-    
+
+    public static ArrayList<Integer> lonelyfinder(ArrayList<Integer> list){
+        ArrayList<Integer> num = new ArrayList<>();
+        ArrayList<Integer> lonelyELement = new ArrayList<>();
+
+        for (Integer repit : list) {
+            if (!num.contains(repit)) {
+                num.add(repit);
+            } else{
+                num.remove(repit);
+            }
+        }
+        for (Integer i : num) {
+            boolean found = false;
+            for (Integer j : num) {
+                if ((j == i -1) || (j == i + 1)) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                lonelyELement.add(i);
+            }
+        }
+    return num;
+}
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(3);
+        list.add(5);
+        list.add(3);
+        for (Integer num : list) {
+            System.out.println(num);
+        }
+        System.out.println(lonelyfinder(list));
+    }
 }
